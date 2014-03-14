1554e3db555043398b5fee50e3aae1c03dd1a5bf
Arnaud Limbourg <arnaud@limbourg.com>
2014-03-14 11:30:56 +0100

    Update middleware.py

--- moat/middleware.py
+++ moat/middleware.py
@@ -17,7 +17,7 @@ import logging
 import re
 
 from django.conf import settings
-from django.http.response import HttpResponseRedirectBase
+from django.http.response import HttpResponse, HttpResponseRedirectBase
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import resolve
 
